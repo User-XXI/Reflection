c035793448001cc088a1c149b967501a2d668aae
User-XXI <andmak96@gmail.com>
2022-11-25 17:55:16 +0300

    Добавьте файлы проекта.

---
+++ AnimalLibrary/AnimalLibrary.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace AnimalLibrary
+{
+    [System.AttributeUsage( System.AttributeTargets.Class )]
+    public class CommentAtt : System.Attribute
+    {
+        public string Comment { get; set; }
+        public CommentAtt( string comment )
+        {
+            Comment = comment;
+        }
+    }
+    public enum eAnimalClassification
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+    public enum eFavouriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+
+    [CommentAtt( "Абстрактный класс для объектов, представляющих животных" )]
+    public abstract class Animal
+    {
+        private eAnimalClassification classification;
+
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public bool HideFromOtherAnimals { get; set; }
+
+        public abstract void SayHello();
+        public abstract eFavouriteFood GetFavouriteFood();
+        public eAnimalClassification GetClassification 
+        { 
+            get => classification; 
+        }
+
+        public Animal( string country, string name, string description, bool hideFromOtherAnimals )
+        {
+            Name = name;
+            Country = country;
+            Description = description;
+            HideFromOtherAnimals = hideFromOtherAnimals;
+        }
+
+        public void Deconstruct( string out_name ) 
+        { 
+            out_name = Name; 
+        }
+        public void Deconstruct( string out_name, string out_desc ) 
+        { 
+            out_name = Name; 
+            out_desc = Description; 
+        }
+        public void Deconstruct( string out_name, string out_desc, string out_count ) 
+        { 
+            out_name  = Name;
+            out_count = Country;
+            out_desc  = Description; 
+        }
+        public void Deconstruct( string out_name, string out_desc, string out_count, bool out_hide ) 
+        { 
+            out_name  = Name;
+            out_count = Country;
+            out_desc  = Description; 
+            out_hide  = HideFromOtherAnimals; 
+        }
+    }
+
+    [CommentAtt( "Класс описания коровы" )]
+    public class Cow : Animal
+    {
+        public Cow( string country, string name, string description, bool hideFromOtherAnimals ) :
+            base( country, name, description, hideFromOtherAnimals )
+        {
+            Name = name;
+            Country = country;
+            Description = description;
+            HideFromOtherAnimals = hideFromOtherAnimals;
+        }
+
+        public override eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Plants;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine( "Мууу\n" );
+        }
+    }
+
+    [CommentAtt( "Класс описания льва" )]
+    public class Lion : Animal
+    {
+        public Lion( string country, string name, string description, bool hideFromOtherAnimals ) : 
+            base( country, name, description, hideFromOtherAnimals )
+        {
+            Name = name;
+            Country = country;
+            Description = description;
+            HideFromOtherAnimals = hideFromOtherAnimals;
+        }
+
+        public override eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Meat;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine( "Рррр\n" );
+        }
+    }
+
+    [CommentAtt( "Класс описания свиньи" )]
+    public class Pig : Animal
+    {
+        public Pig( string country, string name, string description, bool hideFromOtherAnimals ) : 
+            base( country, name, description, hideFromOtherAnimals )
+        {
+            Name = name;
+            Country = country;
+            Description = description;
+            HideFromOtherAnimals = hideFromOtherAnimals;
+        }
+
+        public override eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Everything;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine( "Хрю\n" );
+        }
+    }
+}
---
+++ ConsoleProject/Lab07.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Reflection;
+using AnimalLibrary;
+
+namespace Lab07
+{
+    public class Lab07
+    {
+        static IEnumerable<Type> GetClasses( string nameSpace )
+        {
+            var asm = System.Reflection.Assembly.Load(nameSpace);
+            return asm.GetTypes()
+               .Where( type => type.Namespace == nameSpace );
+            //  .Select(type => type.Name);
+        }
+
+        public static void Main( string[] args )
+        {
+            StreamWriter fout = new StreamWriter(@"..\..\..\..\MyLibrary.xml");
+            fout.WriteLine( "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" );  // windows-1251
+            fout.WriteLine( "<Library>" );
+            fout.WriteLine( "<name>ClassLibrary1</name>" );
+
+            foreach (var MyType in GetClasses( "AnimalLibrary" ))
+            {
+                Console.WriteLine( "typename=" + MyType.Name + ":" );
+                Console.WriteLine( "abstract=" + MyType.IsAbstract + ";" );
+                Console.WriteLine( "public="   + MyType.IsPublic + ";" );
+                Console.WriteLine( "basetype=" + MyType.BaseType.Name + ";" );
+                fout.WriteLine( "\t<type>" );
+                fout.WriteLine( "\t\t<name>" + MyType.Name + "</name>" );
+
+                if (MyType.IsPublic || MyType.IsAbstract)
+                {
+                    fout.Write( "\t\t<modifiers>" );
+                    if (MyType.IsPublic) fout.Write( "public " );
+                    if (MyType.IsAbstract) fout.Write( "abstract" );
+
+                    fout.WriteLine( "</modifiers>" );
+                }
+                fout.WriteLine( "\t\t<basetype>" + MyType.BaseType.Name + "</basetype>" );
+
+                //  foreach (MemberInfo member in MyType.GetMembers())
+                foreach (MemberInfo member in MyType.GetMembers( BindingFlags.DeclaredOnly
+                            |BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static ))
+                {
+                    fout.WriteLine( "\t\t<member>" );
+                    fout.WriteLine( "\t\t\t<name>"   + member.Name + "</name>" );
+                    fout.WriteLine( "\t<membertype>" + member.MemberType + "</membertype>" );
+                    Console.WriteLine( "\tname=" + member.Name );
+                    Console.WriteLine( "\tmembertype=" + member.MemberType );
+
+                    if (member.MemberType == MemberTypes.Field)
+                    {
+                        FieldInfo field = MyType.GetField(member.Name);
+                        if (field != null)
+                        {
+                            Type mytype = field.FieldType;
+                            Console.WriteLine( "\t\ttype=" + mytype.Name );
+                            fout.WriteLine( "\t\t\t<fieldtype>" + mytype.Name + "</fieldtype>" );
+                        }
+                    }
+
+                    if (member.MemberType == MemberTypes.Property)
+                    {
+                        PropertyInfo prop = MyType.GetProperty(member.Name);
+                        if (prop != null)
+                        {
+                            Type mytype = prop.PropertyType;
+                            Console.WriteLine( "\t\ttype=" + mytype.Name );
+                            fout.WriteLine( "\t\t\t<propertytype>" + mytype.Name + "</propertytype>" );
+                        }
+                    }
+
+                    fout.WriteLine( "\t\t</member>" );
+                }
+                Console.WriteLine();
+                fout.WriteLine( "\t</type>" );
+            }
+            fout.WriteLine( "</Library>" );
+            fout.Close();
+        }
+    }
+}
